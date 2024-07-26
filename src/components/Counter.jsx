c75cf1d08cdb2c89f5c04d7002afb9f5ea1ec569
Tanzim <tanzim008@gmail.com>
2024-07-25 23:52:40 -0230

    cretaed buttons in counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <div>Counter</div>
+    <>
+      <div className="flex flex-col justify-center items center">
+        <div>Counter: {counter}</div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Increament
+        </button>
+        <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Decreament
+        </button>
+      </div>
+
+    </>
   )
 }
 
-export default Counter
+export default Counter;
