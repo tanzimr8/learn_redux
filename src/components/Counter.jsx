bbbf78f3f91450a71cb8789f7cf6a99288f9100d
Tanzim <tanzim008@gmail.com>
2024-07-25 23:57:46 -0230

    added handlebutton functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,16 +2,26 @@ import React, { useState } from 'react'
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const add = ()=>{
+    setCounter( prev=> prev+1);
+  }
+  const sub = ()=>{
+    setCounter( prev=> prev-1);
+
+  }
   return (
     <>
-      <div className="flex flex-col justify-center items center">
+      <div className="min-h-screen w-full flex flex-col justify-center items-center space-y-2">
         <div>Counter: {counter}</div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Increament
-        </button>
-        <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Decreament
-        </button>
+        <div className="flex space-x-2">
+          <button onClick={add} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Increament
+          </button>
+          <button onClick={sub} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Decreament
+          </button>
+        </div>
+
       </div>
 
     </>
